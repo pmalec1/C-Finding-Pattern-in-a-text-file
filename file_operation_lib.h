28d794b54d9be44e803e9109e65fe39ddc157def
pmalec1 <01169056@pw.edu.pl>
2022-11-06 23:22:40 +0100

    New header

---
+++ file_operation_lib.h
@@ -0,0 +1,10 @@
+/************DECLARATIONS of functions and constants*************/
+
+#define number_of_expected_ext_args 4 
+
+void show_arguments_of_calling_the_program(int argc,char **argv);
+void check_number_of_arguments(int argc);
+void check_if_file_exist(char **argv);
+
+
+
