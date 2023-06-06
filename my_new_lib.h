92ea71e6d31ec25ea77740cf16de7ece4ebf8e1b
pmalec1 <119669220+pmalec1@users.noreply.github.com>
2023-06-06 15:09:55 +0200

    fixing issues - my_new_lib.h

--- my_new_lib.h
+++ my_new_lib.h
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,5 +11,4 @@ int check_number_of_arguments(int number_of_calling_args);
 int analyse_data_from_from_line (char temp_line[255],char searched_phrase[255],unsigned short aray_of_pattern_positions_in_line[255]);
 void clear_current_line(char current_line[255]);
 void show_line_statistics(unsigned short aray_of_pattern_positions_in_line[255],short line_number);
-
 
